4526845ec494bd058ddf37859e0df2f0f95873fa
kishore2106 <125995784+kishore2106@users.noreply.github.com>
2025-05-01 20:07:59 +0530

    CONNECT INSIDE NETWORK

--- storage-consumer-python/app/config.py
+++ storage-consumer-python/app/config.py
@@ -2,7 +2,6 @@
 
 # Kafka Configuration
 KAFKA_BOOTSTRAP_SERVERS = 'localhost:9092'    # CONNECT INSIDE NETWORK to kafka broker
-# KAFKA_BOOTSTRAP_SERVERS = 'broker:9092'    # CONNECT INSIDE NETWORK to kafka broker
 KAFKA_TOPIC = 'storage_metrics'             # Kafka topic you want to consume
 KAFKA_GROUP_ID = 'ssip-group'                # Kafka consumer group ID
 
@@ -10,3 +9,7 @@ KAFKA_GROUP_ID = 'ssip-group'                # Kafka consumer group ID
 MONGO_URI = 'mongodb://localhost:27017'          # CONNECT INSIDE NETWORK to MongoDB
 MONGO_DB = 'ssip'                            # MongoDB Database name
 MONGO_COLLECTION = 'storage_metrics'         # Collection name
+
+# CONNECT INSIDE NETWORK
+# KAFKA_BOOTSTRAP_SERVERS = 'broker:9092'    # CONNECT INSIDE NETWORK to kafka broker
+# MONGO_URI = 'mongodb://mongo:27017'          # CONNECT INSIDE NETWORK to MongoDB
